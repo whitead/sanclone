67cd6fc2466103eda64402e468ca150674d96a18
Sam Cox <swrig30@ur.rochester.edu>
2023-10-06 16:22:55 -0700

    forgot to change prompt, oops

--- sanclone/agent/prompt.py
+++ sanclone/agent/prompt.py
@@ -1,30 +1,8 @@
 # flake8: noqa
-PREFIX = """
+FORMAT_INSTRUCTIONS = """
 You are an expert in molecular cloning.
 You have a set of tools at your disposal.
 Your task is to respond to the question or
 solve the problem to the best of your ability using the provided tools.
-"""
-
-
-FORMAT_INSTRUCTIONS = """
-You are an expert in molecular cloning. Answer the question using the tools provided.
-
-You can only respond with a single complete
-"Thought, Action, Action Input" format
-OR a single "Final Answer" format.
-
-Complete format:
-
-Thought: (reflect on your progress and decide what to do next)
-Action: (the action name)
-Action Input: (the input string to the action)
-
-OR
-
-Final Answer: (the final answer to the original input question)
-
-Your final answer should contain all information necessary to answer the question and subquestions.
-Your thought process should be clean and clear, and you must explicitly state the actions you are taking.
-Question: {input}
+Here is the question: {input}
 """
