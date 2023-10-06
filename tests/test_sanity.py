411d0cfeb5af50e9a5162de1d5cab470d562cde7
Andrew White <white.d.andrew@gmail.com>
2023-10-06 15:28:15 -0700

    Added example tool

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -3,3 +3,10 @@ import sanclone
 
 def test_version():
     assert sanclone.__version__
+
+def test_echo_tool():
+    from sanclone.tools import EchoTool
+    from sanclone import State
+    tool = EchoTool(shared_state=State())
+    assert tool.run("Hello") == "Hello"
+    
